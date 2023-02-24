74945992d9b1dd25fe7169b9757094543021aa0f
Lorraine Ogolla <kisangelorraine@gmail.com>
2023-02-23 16:34:14 -0800

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,15 @@
 #include "main.h"
 
-
 /**
- * print_number - prints an integer
- * @n: integer to be printed
- */
-void print_number(int n)
-{
-	unsigned int n1;
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	}
-	else
-	{
-		n1 = n;
-	}
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
