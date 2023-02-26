1646878f093a3beb1decd128641f5208227aca11
Lorraine Ogolla <kisangelorraine@gmail.com>
2023-02-26 04:43:13 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,9 +21,11 @@ int main(void)
 					putchar(j + '0');
 					putchar(k + '0');
 				if (i + j + k != 24)
-					putchar(',');
-				putchar(' ');
+				{	putchar(',');
+					putchar(' ');
 				}
+				}
+
 			}
 		}
 	}
