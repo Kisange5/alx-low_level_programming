82111be5f57a3dabe0e9f0835a93c2d305504b3c
Lorraine Ogolla <kisangelorraine@gmail.com>
2023-03-03 12:09:09 -0800

    A function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,36 +1,24 @@
 #include "main.h"
-
 /**
- * leet - a function that encodes a string to 1337
- * @a and @A: should be replaced by 4
- * @e and @E: should be replaced by 3
- * @o and @O: should be replaced by 0
- * @t and @T: should be replaced by 7
- * @l and L: should be replaced by 1
- * Return: leet_str
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
  */
-char *leet(char *)
+char *leet(char *n)
 {
-	int i = str *i;
-	int j = str *j;
-	char leet_str;
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for ('i' = 0, 'j' = 0; str[i] != '\0'; i++, j++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			leet_str[j] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			leet_str[j] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			leet_str[j] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			leet_str[j] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			leet_str[j] = '1';
-		else
-			leet_str[j] = str[i];
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
 	}
-	leet_str[j] = '\0';
-	return (leet_str);
+	return (n);
 }
-
