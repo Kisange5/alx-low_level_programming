d9517a08bb4066cef93571407da8ef70dd00ec8a
Lorraine Ogolla <kisangelorraine@gmail.com>
2023-03-10 13:16:47 -0800

    Static library

---
+++ 0x09-static_libraries/2-strncopy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+char *_strncopy(char *dest, char *src, int n)
+{
+	return strncpy(dest, src, n);
+}
