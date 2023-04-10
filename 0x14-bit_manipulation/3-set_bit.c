462bbfdb987c2b939cc68319f2ccf2ba7b073e26
Lorraine Ogolla <kisangelorraine@gmail.com>
2023-04-10 02:13:53 -0700

    Hot fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,8 +9,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned int) * 8))
+	if (index > 63)
 		return (-1);
-	*n = *n | (1 << index);
-	return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
 }
