0661076327a2d1771d0879237e947fac29013145
Kisange5 <kisangelorraine@gmail.com>
2023-06-15 14:01:21 -0700

    An update on the main file

--- 0x17-doubly_linked_lists/0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - A  function that prints all the elements of a dlistint_t list.
+ * print_dlistint - prints all the elements of a dlistint_t list.
  * @h: the head of the linked lists
  * Return: the number of node
  */
@@ -17,5 +17,5 @@ size_t print_dlistint(const dlistint_t *h)
 		h = h->next;
 		count++;
 	}
-	return(count);
+	return (count);
 }
